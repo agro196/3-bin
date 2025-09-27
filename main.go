168e4069d7789d767dea1bb01d7697ef0963ae5e
Andrey Andreev <agro_06@mail.ru>
2025-09-27 21:21:31 +0300

    Создан bin и binList

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"time"
+)
+
+type bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+type binList []bin
+
+func newBin(id string, private bool, name string) bin {
+	return bin{
+		id:        id,
+		private:   private,
+		createdAt: time.Now(),
+		name:      name,
+	}
+}
+
 func main() {
 
 }
